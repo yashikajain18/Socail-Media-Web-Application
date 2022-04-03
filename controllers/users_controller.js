a683af55c16c507b52d4a8afe30a5d78cd0da198
yashika jain <yashikajain@yashikas-MacBook-Air.local>
2022-04-04 04:55:11 +0530

    create user/sign-up

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,52 @@
+const User = require('../models/user');
+
 module.exports.profile=function(req, res){
-    res.end('<h1> User Profile</h1>');
+    // res.end('<h1> User Profile</h1>');
+    return res.render('user_profile', {
+        title: "User Profile"
+    });
+}
+
+// render sign in page
+module.exports.signIn=function(req, res){
+    return res.render('user_sign_in', {
+        title:"Codeial | Sign In"
+    });
+}
+// render sign up page
+module.exports.signUp=function(req, res){
+    return res.render('user_sign_up', {
+        title: " Codeial | Sign Up"
+    });
+}
+
+//get the sign up data
+
+module.exports.create =function(req, res){
+    if(req.body.password != req.body.confirm_password){
+        //console.log('1st');
+        return res.redirect('back');
+     }
+    
+    User.findOne({email: req.body.email}, function(err, user){
+        console.log(user);
+        if(err){console.log('error in finding user in signing up'); return}
+
+        if(!user){
+            //console.log('3rd');
+            User.create(req.body, function(err, user){
+                if(err){console.log('error in creating user while signing up', err); return}
+
+                return res.redirect('/users/sign-in');
+            });
+        }else{
+            //console.log('4th');
+            return res.redirect('back');
+        }
+    });
+}
+
+// sign in and create the session for the user
+module.exports.createSession=function(req, res){
+    //TODO later
 }
--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 const express =require('express');
+const cookieParser = require('cookie-parser');
 const app = express();
 const port =8000;
+const expressLayouts = require('express-ejs-layouts');
+const db =require('./config/mongoose');
+
+app.use(express.urlencoded());
+
+app.use(cookieParser());
+//use express layout 
+app.use(expressLayouts);
+
+// extract style and scripts from subpages into layout 
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true)
+
+app.use(express.static('./assets'));
 
 //use express router
-
 app.use('/', require('./routes'));
 
 // set up the view engine
 app.set('view engine', 'ejs');
 app.set('views', './views');
+
+
 app.listen(port, function(err){
      if(err){
         //  console.log(err);
