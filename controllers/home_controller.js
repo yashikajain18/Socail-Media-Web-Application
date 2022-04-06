f716620986014e8957892b568e6d83d814678f1b
yashika jain <yashikajain@yashikas-MacBook-Air.local>
2022-04-06 15:45:10 +0530

    display each post

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,7 +1,18 @@
+const Post = require('../models/post')
 module.exports.home = function(req, res){
-      console.log(req.cookies);  
-    //return res.end('<h1> Express is up for codeial</h1>');
-    return res.render('home', {
-           title: "Home"
-    });
+      //console.log(req.cookies);  
+//     Post.find({}, function(err, posts){
+//        return res.render('home', {
+//               title: "Codeial | Home",
+//               posts: posts
+//        });
+//     })
+
+//populate the user of each post
+    Post.find({}).populate('user').exec(function(err, posts){
+       return res.render('home', {
+              title: "Codeial | Home",
+              posts: posts
+       });
+    })
 }
