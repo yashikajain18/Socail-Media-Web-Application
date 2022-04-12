71ff9e0c3442a2516316d00609d32a5af2a0cf0a
yashika jain <yashikajain@yashikas-MacBook-Air.local>
2022-04-13 01:17:13 +0530

    set up socket.io and establish connection

---
+++ config/chat_sockets.js
@@ -0,0 +1,17 @@
+module.exports.chatSockets = function(socketServer){
+    let io = require('socket.io')(socketServer, {
+        cors: {
+            origin: 'http://localhost:8000',
+          },
+    });
+
+    io.sockets.on('connection', function(socket){
+        console.log('new connection received', socket.id);
+
+        socket.on('disconnect', function(){
+            console.log('socket disconnected!');
+        });
+
+    });
+
+}
--- index.js
+++ index.js
@@ -16,6 +16,12 @@ const sassMiddleware = require('node-sass-middleware');
 const flash = require('connect-flash');
 const customMware=require('./config/middleware');
 
+// set up chat server to be used with socket.io 
+const chatServer =require('http').Server(app);
+const chatSockets=require('./config/chat_sockets').chatSockets(chatServer);
+chatServer.listen(5000);
+console.log('chat server is running on port 5000');
+
 app.use(sassMiddleware({
    src: './assets/scss',
    dest: './assets/css',
